15066c6f0841d5c00c9adc9b4e9cdf0a28c1046c
Weverton Ryan <wevertonryan205@gmail.com>
2024-08-28 23:08:58 -0300

    initial commit

---
+++ Boucing_ball.lua
@@ -0,0 +1,30 @@
+local frame = {
+  "|•                            |\n|                             |\n|                             |\n|                             |\n|                             |\n|                             |",
+  "|  .                          |\n|                             |\n|                             |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|   •                         |\n|                             |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|    `                        |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                             |\n|       -                     |",
+  "|                             |\n|                             |\n|                             |\n|        /                    |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|          .                  |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|            •                |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|              .              |\n|                             |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|                             |\n|                •            |\n|                             |\n|                             |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                             |\n|                  -          |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                   .         |\n|                             |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                    .        |\n|                             |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                             |\n|                     -       |",
+  "|                             |\n|                             |\n|                             |\n|                             |\n|                             |\n|                      °      |",
+}
+
+local function wait(t)
+    local time = os.clock() + t
+    repeat until os.clock() >= time
+end
+
+while 1==1 do
+    for i = 1, 15 do
+        wait(0.09)
+        print("\x1b[2J\x1b[1;1H")
+        print(frame[i])
+    end
+end
